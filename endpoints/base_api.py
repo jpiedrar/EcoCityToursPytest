5e3642c9fd795b085a888bd927f6c66a07fd56ff
Juan Piedra <juapiero@gmail.com>
2026-09-06 22:43:59 -0600

    Adding BaseAPIClient

---
+++ endpoints/base_api.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+class BaseAPIClient:
+    DEFAULT_BASE_URL = os.getenv('API_BASE_URL', 'mockedapi.com/api/')
+    DEFAULT_TIMEOUT = float(os.getenv('API_TIMEOUT', '10'))
+
+    def __init__(self, base_url=None, timeout=None):
+        self.base_url = (base_url or self.DEFAULT_BASE_URL).rstrip('/') + '/'
+        self.timeout = timeout if timeout is not None else self.DEFAULT_TIMEOUT
+
+    def build_url(self, path=''):
+        return f"{self.base_url}{path.lstrip('/')}"
+
+    def post(self, path='', **kwargs):
+        return requests.post(
+            self.build_url(path),
+            timeout=self.timeout,
+            **kwargs,
+        )
